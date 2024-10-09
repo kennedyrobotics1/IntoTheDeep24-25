1d587281e2ac4594b4d48b45df3b8d43f38fca5f
isabellammjfk <izzysoccer14@icloud.com>
2024-10-09 12:16:33 -0700

    current intake code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/IntoTheDeepTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/IntoTheDeepTeleOp.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode.Teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.robotcontroller.external.samples.BasicOpMode_Iterative;
 
@@ -14,13 +16,21 @@ public class IntoTheDeepTeleOp extends BasicOpMode_Iterative {
     private DcMotor leftBack = null;
     private DcMotor rightBack = null;
 
+    private Servo intake;
+    // private CRServo intake;
+
     double frontLeftPower, frontRightPower, backLeftPower, backRightPower;
+
+    double intakePosition;
 
     public void init() {
         leftFront = hardwareMap.get(DcMotor.class, "leftFront");
         rightFront = hardwareMap.get(DcMotor.class, "rightFront");
         leftBack = hardwareMap.get(DcMotor.class, "leftBack");
         rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+
+        intake = hardwareMap.get(Servo.class, "servo1");
+        // intake = hardwareMap.get(CRServo.class, "servo1");
 
         leftFront.setDirection(DcMotor.Direction.REVERSE);
         rightFront.setDirection(DcMotor.Direction.FORWARD);
@@ -46,6 +56,13 @@ public class IntoTheDeepTeleOp extends BasicOpMode_Iterative {
         backLeftPower   = (y - x + r) / denominator;
         backRightPower  = (y + x - r) / denominator;
 
+        if (gamepad1.x) {
+            intake.setPosition(1.0);
+        } else if (gamepad1.b) {
+            intake.setPosition(-1.0);
+        } else {
+            intake.setPosition(0.0);
+        }
 
         if(gamepad1.left_bumper){
             leftFront.setPower(0.5 * frontLeftPower);
@@ -63,7 +80,6 @@ public class IntoTheDeepTeleOp extends BasicOpMode_Iterative {
             telemetry.addData("backLeftPower ", backLeftPower);
             telemetry.addData("backRightPower ", backRightPower);
             telemetry.update();
-
         }
     }
 }
