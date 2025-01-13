ffd7f0b6c85408fada731f1a56a537b2a65b9f76
AkithMadurapperuma <wildmalzoo@gmail.com>
2025-01-13 14:26:42 -0800

    I started the addition to a 1 Specimen Score and Push 3 Samples into observation.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/HighSpecimenPushBot3Auto.java
@@ -0,0 +1,90 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+// RR-specific imports
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ParallelAction;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.SleepAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+
+// Non-RR imports
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+
+@Config
+@Autonomous(name = "HighSpecimenPushBot3Auto", group = "23393 Auto")
+public class HighSpecimenPushBot3Auto extends LinearOpMode {
+
+    private SlidesRotationClass slideRotation;
+    private ExtensionClass extensionMotor;
+    private IntakeWristClass wrist;
+    private ClawClass claw;
+
+    @Override
+    public void runOpMode() {
+
+        slideRotation = new SlidesRotationClass(hardwareMap);
+        extensionMotor = new ExtensionClass(hardwareMap, telemetry);
+        wrist = new IntakeWristClass(hardwareMap, telemetry);
+        claw = new ClawClass(hardwareMap);
+
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 60, Math.toRadians(90)));
+
+        Action MoveToHighBar = drive.actionBuilder(drive.pose)
+                .strafeTo(new Vector2d(0, 30))
+                .build();
+
+        Action MoveBackToPlaceSpecimen = drive.actionBuilder(drive.pose)
+                .strafeTo(new Vector2d(-35, 55))
+                .build();
+
+        Action Push3IntoObservation = drive.actionBuilder(drive.pose)
+                .strafeTo(new Vector2d(-35, 12))
+
+                .strafeTo(new Vector2d(-42, 12))
+
+                .strafeTo(new Vector2d(-42, 55))
+
+                .strafeTo(new Vector2d(-42, 12))
+
+                .strafeTo(new Vector2d(-54, 12))
+
+                .strafeTo(new Vector2d(-54, 53))
+
+                .strafeTo(new Vector2d(-54, 12))
+
+                .strafeTo(new Vector2d(-62, 12))
+
+                .strafeTo(new Vector2d(-62, 55))
+                        .build();
+
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+
+        Actions.runBlocking(new SequentialAction(
+                new ParallelAction(
+                        MoveToHighBar,
+                        slideRotation.highBarSpecimen(),
+                        extensionMotor.highBarSpecimen(),
+                        claw.close()
+                ),
+                new SequentialAction(
+                        extensionMotor.specimenHighBarOuttake()
+                ),
+                new SequentialAction(
+                        Push3IntoObservation
+                )
+
+        ));
+
+    }
+}
