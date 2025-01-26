729368e0d39bdbd3a5e76c5fdb9b1cdd11fbc060
atra-ment <makevina000@gmail.com>
2025-01-25 18:46:48 -0800

    added wrist touch sensor declarations back in

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/IntoTheDeepTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop/IntoTheDeepTeleOp.java
@@ -38,6 +38,8 @@ public class IntoTheDeepTeleOp extends BasicOpMode_Iterative {
 
     private static final double TICKSPERINCH = 75.71;
 
+    private DigitalChannel wristTouch;
+
 
     public void init() {
         leftFront = hardwareMap.get(DcMotor.class, "leftFront");
@@ -64,6 +66,9 @@ public class IntoTheDeepTeleOp extends BasicOpMode_Iterative {
 
         armPosition = new ServoController(0.35);
         setArmRotationPosition(armPosition.position);
+
+        wristTouch = hardwareMap.get(DigitalChannel.class, "sensor1e");
+        wristTouch.setMode(DigitalChannel.Mode.INPUT);
 
         leftFront.setDirection(DcMotor.Direction.REVERSE);
         rightFront.setDirection(DcMotor.Direction.FORWARD);
